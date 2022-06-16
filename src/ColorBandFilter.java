d5f314ac3291828a4735d2b2f23020b4e5ad0e77
Shadow78901 <107639246+Shadow78901@users.noreply.github.com>
2022-06-16 23:14:35 +0200

    Update ColorBandFilter.java
    Kommentare wurden hinzugefügt und die Struktur wurde nochmal neue formatiert.

--- src/ColorBandFilter.java
+++ src/ColorBandFilter.java
@@ -1,27 +1,40 @@
 import java.awt.image.BufferedImage;
 
+/**
+ * Ein ColorBandFilter durch den das Bild auf einen Farbwert reduziert wird
+ * 
+ * @author Artur Kechter, Nico Hunsicker, Atta Farsimadan
+ *
+ */
 public class ColorBandFilter extends PixelFilter {
-	
+
 	private ColorBand color;
+
 	@Override
-	public BufferedImage process(String value, BufferedImage ...image) {
+	/**
+	 *Diese Methode wird benutzt um das Bild zu filtern
+	 *
+	 *@param value der Farbwert auf den das Bild reduziert werden soll
+	 *@param image ein Array von BufferedImages, das benutzt wird
+	 *@return das gefiltertete Bild
+	 */
+	public BufferedImage process(String value, BufferedImage... image) {
 		int width = image[0].getWidth();
 		int height = image[0].getHeight();
 		int rgbDec;
 		BufferedImage result = new BufferedImage(width, height, BufferedImage.TYPE_3BYTE_BGR);
 		int rgbDecResult;
-		
-		if ( value.equals("red") || value.equals("Red")) {
+
+		if (value.equals("red") || value.equals("Red")) {
 			color = ColorBand.RED;
-		} else if( value.equals("blue") || value.equals("Blue")) {
+		} else if (value.equals("blue") || value.equals("Blue")) {
 			color = ColorBand.BLUE;
-		} else if( value.equals("green") || value.equals("Green") ) {
+		} else if (value.equals("green") || value.equals("Green")) {
 			color = ColorBand.GREEN;
 		} else {
 			System.out.println("no color choosen");
 		}
-		
-		
+
 		for (int i = 1; i < width; i++) {
 			for (int j = 1; j < height; j++) {
 				rgbDec = image[0].getRGB(i, j);
@@ -30,13 +43,19 @@ public class ColorBandFilter extends PixelFilter {
 
 			}
 		}
-		
+
 		System.out.println(value);
 		return result;
 	}
 
+	/**
+	 * 
+	 * @param rgbDec Ein RGB Wert wird in seiner Dezimal Variante mitgegeben
+	 * @param color Die Farbe auf die das Bild reduziert werden soll
+	 * @return der neue Farbwert als Dezimalzahl
+	 */
 	private int determineColor(int rgbDec, ColorBand color) {
-		
+
 		String rgbBin = Integer.toBinaryString(rgbDec);
 		String resultBin = "";
 		final String emptyByte = "00000000";
