fab7d17dfcec4c3e3efb096b7737ed57b51394cc
Shadow78901 <107639246+Shadow78901@users.noreply.github.com>
2022-06-16 23:32:12 +0200

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -4,23 +4,34 @@ import java.io.IOException;
 
 import javax.imageio.ImageIO;
 
+/**
+ * Der Controller mit dem die Filter und Bilder aufgerufen werden
+ * 
+ * @author Artur Kechter, Nico Hunsicker, Atta Farsimadan
+ *
+ */
 public class Controller {
+	/**
+	 * Die Main Methode
+	 * 
+	 * @param args Die args werden berücksichtigt
+	 */
 	public static void main(String[] args) {
 		BufferedImage picture = null;
 		BufferedImage result = null;
-		BufferedImage mask = null; //ak
-		
+		BufferedImage mask = null;
+
 		String filtername = args[0];
 		String filtervalue = args[1];
 		String inputImage = args[2];
 		String outputImage = args[3];
-		
+
 		Filter filter;
-		
-		if ( args.length >= 4) {
+
+		if (args.length >= 4) {
 			String maskImage = args[4];
 			System.out.println(args[4]);
-			
+
 			try {
 				mask = ImageIO.read(new File(maskImage));
 			} catch (IOException e) {
@@ -31,10 +42,9 @@ public class Controller {
 		} else {
 			System.out.println("no mask");
 		}
-		
 
-		//filter werden initialisiert
-		
+		// filter werden initialisiert
+
 		if (filtername.equals("ColorBandFilter")) {
 			filter = new ColorBandFilter();
 		} else if (filtername.equals("MonochromeFilter")) {
@@ -43,22 +53,21 @@ public class Controller {
 			filter = new ThresholdFilter();
 		}
 
-		// hier muss Hashmap hin 
-		
-		
+		// hier muss Hashmap hin
+
 		// file wird eingelesen
-		
+
 		try {
 			picture = ImageIO.read(new File(inputImage));
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		//neues bild wird mit ausgewählten filter gebaut
-	
-		
-		result = filter.process(filtervalue, picture, mask); // Value, Das Originalbild sowie Maske werden an Filter übergeben
+
+		// neues bild wird mit ausgewählten filter gebaut
+
+		result = filter.process(filtervalue, picture, mask); // Value, Das Originalbild sowie Maske werden an Filter
+																// übergeben
 		try {
 			ImageIO.write(result, "bmp", new File(outputImage));
 		} catch (IOException e1) {
